07f8d8bb1f36afb3b910188a949adcd33e593b56
jzwart <jzwart@usgs.gov>
2018-05-02 09:45:01 -0500

    changing to rds

--- 1_data/src/pull_nwis.R
+++ 1_data/src/pull_nwis.R
@@ -8,10 +8,8 @@ pull_nwis <- function(ind_file, sites_yml, dates_yml, params_yml, gd_config) {
   flow <- readNWISuv(siteNumbers=site_ids, parameterCd=pcodes$flow, startDate=dates$calibrate$start, endDate=dates$forecast$start)
   nitrate <- readNWISuv(siteNumbers=site_ids, parameterCd=pcodes$nitrate, startDate=dates$calibrate$start, endDate=dates$forecast$start)
 
-  writeLines(list(flow=flow, nitrate=nitrate), outfile)
-
   data_file <- as_data_file(ind_file)
-  writeLines(list(flow = flow, nitrate = nitrate), data_file)
+  saveRDS(list(flow = flow, nitrate = nitrate), data_file)
   gd_put(remote_ind = ind_file, local_source = data_file, config_file = gd_config)
 }
 
