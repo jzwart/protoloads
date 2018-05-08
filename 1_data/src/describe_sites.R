3d8c7b1b3d303eca105a8a8c93a1114faf15f125
Alison Appling <aappling@usgs.gov>
2018-05-08 17:03:13 -0500

    refine site description function/workflow

--- 1_data/src/describe_sites.R
+++ 1_data/src/describe_sites.R
@@ -1,4 +1,4 @@
-describe_sites <- function(ind_file, sites_comids_ind, pcodes, remake_file) {
+describe_sites <- function(ind_file, sites_comids_ind, pcodes, remake_file, gd_config) {
 
   # read the comid-site table. don't bother filtering to the final 3 sites, just prep all 8
   comids <- readr::read_tsv(sc_retrieve(sites_comids_ind, remake_file=remake_file))
@@ -15,15 +15,20 @@ describe_sites <- function(ind_file, sites_comids_ind, pcodes, remake_file) {
   }
 
   # go to NWIS for site details, then join with COMIDs
-  info <- lapply(pcodes, function(pcode) {
-    readNWISInfo(nwis_site, pcode, interactive=FALSE)
+  site_info <- lapply(comids$site_id, function(nwis_site) {
+    lapply(pcodes, function(pcode) {
+      readNWISInfo(nwis_site, pcode, interactive=FALSE)
+    }) %>%
+      bind_rows() %>%
+      mutate(project_no = as.character(project_no)) %>%
+      summarize_all(.funs=funs(unique_or_concat))
   }) %>%
     bind_rows() %>%
-    summarize_all(.funs=funs(unique_or_concat)) %>%
     left_join(comids, by=c(site_no='site_id'))
 
+  # write and post the compiled site/parameter information
   data_file <- scipiper::as_data_file(ind_file)
-  readr::write_tsv(info, data_file)
-  gd_put(ind_file, data_file=data_file)
+  readr::write_tsv(site_info, data_file)
+  gd_put(ind_file, data_file, config_file=gd_config)
 
 }
