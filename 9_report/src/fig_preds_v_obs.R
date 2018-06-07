96aa559b03cea9c6931e5f38028a7fedfdd49795
jzwart <jzwart@usgs.gov>
2018-06-07 14:26:35 -0500

    adding figure config #72

--- 9_report/src/fig_preds_v_obs.R
+++ 9_report/src/fig_preds_v_obs.R
@@ -1,32 +1,40 @@
 fig_preds_v_obs <- function(fig_ind, loadest_preds_ind, wrtds_preds_ind, agg_nwis_ind, remake_file, config_file){
   # read in the predictions
   loadest_preds_df <- readRDS(sc_retrieve(loadest_preds_ind, remake_file)) %>%
-    mutate(LeadTime = as.numeric(Date - ref_date, units='days'))
+    select('Date','Flux','ref_date','Site','model_range')
+
   wrtds_preds_df <- readRDS(sc_retrieve(wrtds_preds_ind, remake_file)) %>%
-    mutate(LeadTime = as.numeric(Date - ref_date, units='days'))
+    select('Date', 'Flux', 'ref_date', 'Site', 'model_range')
 
   # read in "truth"
   agg_nwis <- readRDS(sc_retrieve(agg_nwis_ind, remake_file))
   agg_nwis$flux <- left_join(agg_nwis$nitrate_sensor, agg_nwis$flow, by=c('site_no','date'), suffix=c('_conc','_flow')) %>%
     mutate(daily_mean_flux = daily_mean_conc * daily_mean_flow * 60*60*24/1000) %>% # flow in kg/d
-    rename(site=site_no)
+    rename(site=site_no) %>%
+    select('site', 'date', 'daily_mean_flux')
 
   # join predictions and obs together
   preds_obs <- left_join(loadest_preds_df, wrtds_preds_df, by = c('Date', 'ref_date', 'Site', 'model_range'), suffix = c('_loadest', '_wrtds')) %>%
+    gather(key = 'flux_model', value = 'pred_flux', starts_with('Flux')) %>%
+    mutate(LeadTime = as.numeric(Date - ref_date, unit = 'days')) %>%
     left_join(y = agg_nwis$flux, by = c('Site' = 'site', 'Date' = 'date')) %>%
-    dplyr::filter(!is.na(daily_mean_flux), !is.na(Flux_loadest), !is.na(Flux_wrtds))
+    dplyr::filter(!is.na(daily_mean_flux), !is.na(pred_flux)) %>%
+    rename(obs_flux = daily_mean_flux)
 
 
   # create the plot
-  g <- ggplot(preds_obs) +
-    geom_point(aes(x = daily_mean_flux/1000, y = Flux_loadest/1000, color = 'orange')) +
-    geom_point(aes(x = daily_mean_flux/1000, y = Flux_wrtds/1000, color = 'lightblue'))
-
-
-  , aes(x=daily_mean_flux, y=Flux/1000, color=LeadTime)) +
+  g <- ggplot(preds_obs, aes(x = obs_flux/1000, y = pred_flux/1000, color = flux_model)) +
     geom_point() +
-    geom_line(data=filter(agg_nwis$flux, date %in% preds_df$Date), aes(x=date, y=daily_mean_flux/1000), color='red') +
-    facet_grid(site ~ ., scale='free_y') +
+    geom_abline(slope = 1, intercept = 0) +
+    facet_wrap(~Site, scales='free', nrow = 3, ncol = 1,
+               strip.position = 'right') +
+    xlab('Observed Flux') +
+    ylab('Predicted Flux') +
+    theme(legend.title = element_blank()) +
+    theme_classic()
+  g
+
+
     scale_color_continuous('Lead Time (d)') +
     xlab('Date') +
     ylab(expression('Flux'~(Mg~'N-NO'[3]~d^{-1}))) +
