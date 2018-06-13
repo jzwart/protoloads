c9958ea9da8b29368974e5116ef9ec5c922bd5fb
jzwart <jzwart@usgs.gov>
2018-06-13 17:08:40 -0500

    building figure

--- 9_report/src/fig_exceedance.R
+++ 9_report/src/fig_exceedance.R
@@ -1,6 +1,11 @@
-fig_exceedance <- function(fig_ind, config_fig_yml, preds_ind, agg_nwis_ind, remake_file, config_file) {
+fig_exceedance <- function(fig_ind, config_fig_yml, exceed_cfg_yml, preds_ind, agg_nwis_ind, remake_file, config_file) {
   # read in figure scheme config
   fig_config <- yaml::yaml.load_file(config_fig_yml)
+
+  # exceedance thresholds
+  exceed_thresh <- yaml::yaml.load_file(exceed_cfg_yml) %>%
+    bind_rows() %>%
+    gather(key = 'site', value = 'flux_threshold')
 
   # predictions
   preds_df <- readRDS(sc_retrieve(preds_ind, remake_file)) %>%
@@ -12,8 +17,8 @@ fig_exceedance <- function(fig_ind, config_fig_yml, preds_ind, agg_nwis_ind, rem
     mutate(daily_mean_flux = daily_mean_conc * daily_mean_flow * 60*60*24/1000) %>% # flow in kg/d
     rename(site=site_no)
 
-  # error
-  cumulative_freq <- left_join(agg_nwis$flux, preds_df,
+  # cumulative frequency
+  prob_exceed <- left_join(agg_nwis$flux, preds_df,
                         by = c('site' = 'Site', 'date' = 'Date'),
                         suffix = c('_truth', '_pred')) %>%
     dplyr::filter(!is.na(Flux), !is.na(daily_mean_flux)) %>%
@@ -22,109 +27,85 @@ fig_exceedance <- function(fig_ind, config_fig_yml, preds_ind, agg_nwis_ind, rem
     group_by(site) %>%
     arrange(daily_mean_flux, .by_group = T) %>%
     mutate(freq = seq(1,n())/n()) %>%
+    ungroup() %>%
+    left_join(y = exceed_thresh, by ='site') %>%
+    mutate(obs_exceeded = case_when(daily_mean_flux/1000 < flux_threshold ~ 'no',
+                                    TRUE ~ 'yes'),
+           pred_exceeded = case_when(Flux/1000 < flux_threshold ~ 'no',
+                                     TRUE ~ 'yes')) %>%
+    group_by(site, date) %>%
+    mutate(prob_exceed = sum(pred_exceeded == 'yes')/n()) %>%
     ungroup()
 
+    # probability of exceeding is counting how many times forecasts exceeded threshold / number of forecasts
+
   # create the plot
-  g <- ggplot(cumulative_freq, aes(x = daily_mean_flux/1000, y = freq)) +
-    geom_point() +
+  # g <- ggplot(dplyr::filter(cumulative_freq, site == '07374000'), aes(x = daily_mean_flux/1000, y = freq, color = exceeded)) +
+  #   geom_line(size = 1.2) +
+  #   theme(legend.title = element_blank(),
+  #         panel.grid.major = element_blank(),
+  #         panel.grid.minor = element_blank(),
+  #         panel.background = element_blank(),
+  #         axis.line = element_line(colour = "black"),
+  #         legend.position = c(.15,.90),
+  #         legend.key = element_blank(),
+  #         strip.background = element_blank()) +
+  #   scale_color_manual(values = c('no' = 'black',
+  #                                 'yes' = 'red'),
+  #                      labels = c('Below','Exceeded')) +
+  #   facet_wrap(~site, scales='free_x', nrow = 1, ncol = 3,
+  #              strip.position = 'top') +
+  #   xlab(expression(Observed~nitrate~flux~(Mg~N~day^-1))) +
+  #   ylab(expression(Cumulative~Frequency)) +
+  #   geom_vline(aes(xintercept=flux_threshold), color='red', linetype= 'dashed', dplyr::filter(exceed_thresh, site =='07374000')) # adding threshold
+  #
+  # g
+
+  g <- ggplot(dplyr::filter(prob_exceed, site == '07374000'), aes(x = daily_mean_flux/1000, y = prob_exceed)) +
+    geom_line(size = 1.2) +
     theme(legend.title = element_blank(),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           panel.background = element_blank(),
           axis.line = element_line(colour = "black"),
-          legend.position = c(.05,.90),
+          legend.position = c(.15,.90),
           legend.key = element_blank(),
           strip.background = element_blank()) +
-    facet_wrap(~site, scales='free', nrow = 1, ncol = 3,
+    # scale_color_manual(values = c('no' = 'black',
+    #                               'yes' = 'red'),
+    #                    labels = c('Below','Exceeded')) +
+    facet_wrap(~site, scales='free_x', nrow = 1, ncol = 3,
                strip.position = 'top') +
     xlab(expression(Observed~nitrate~flux~(Mg~N~day^-1))) +
-    ylab(expression(Predicted~nitrate~flux~(Mg~N~day^-1)))
-  g
+    ylab(expression(Probability~of~threshold~exceedance)) +
+    geom_vline(aes(xintercept=flux_threshold), color='red', size = 1.3, linetype= 'dashed', dplyr::filter(exceed_thresh, site =='07374000')) # adding threshold
 
+  # g
+  #
+  # g <- ggplot(prob_exceed, aes(x = daily_mean_flux/1000, y = prob_exceed)) +
+  #   geom_line(size = 1.2) +
+  #   theme(legend.title = element_blank(),
+  #         panel.grid.major = element_blank(),
+  #         panel.grid.minor = element_blank(),
+  #         panel.background = element_blank(),
+  #         axis.line = element_line(colour = "black"),
+  #         legend.position = c(.15,.90),
+  #         legend.key = element_blank(),
+  #         strip.background = element_blank()) +
+  #   # scale_color_manual(values = c('no' = 'black',
+  #   #                               'yes' = 'red'),
+  #   #                    labels = c('Below','Exceeded')) +
+  #   facet_wrap(~site, scales='free_x', nrow = 1, ncol = 3,
+  #              strip.position = 'top') +
+  #   xlab(expression(Observed~nitrate~flux~(Mg~N~day^-1))) +
+  #   ylab(expression(Cumulative~Frequency)) +
+  #   geom_vline(aes(xintercept=flux_threshold), color='red', linetype= 'dashed', exceed_thresh) # adding threshold
+  #
+  # g
 
-  # standardized flux error
-  g1 <- ggplotGrob(ggplot(error_df[error_df$site==sites[1],], aes(x=factor(LeadTime), y=std_flux_error, fill = flow_class)) +
-                     geom_hline(yintercept = 0,
-                                linetype = 'dashed') +
-                     geom_boxplot(outlier.shape = NA,
-                                  width = 0.7) +
-                     scale_fill_manual(name = 'flow_class',
-                       values = c('high' = fig_config$flow_class$high,
-                                  'med' = fig_config$flow_class$med,
-                                  'low' = fig_config$flow_class$low),
-                       labels = c('High Flow', 'Medium Flow', 'Low Flow')) +
-                     theme_classic()+
-                     ylim(boxplot.stats(error_df$std_flux_error[error_df$site==sites[1]])$stats[c(1,5)]) +
-                     scale_x_discrete(limits = rev(levels(factor(error_df$LeadTime[error_df$site==sites[1]])))) +
-                     theme(axis.title.x = element_blank(),
-                           axis.title.y = element_blank(),
-                           legend.position = 'none',
-                           plot.margin = unit(c(1,3,1,1),'lines'))+
-                     annotation_custom(grob = textGrob(label = sites[1], hjust = 0, rot = 270),
-                                       ymin = diff(boxplot.stats(error_df$std_flux_error[error_df$site==sites[1]])$stats[c(1,5)])/2,
-                                       ymax = diff(boxplot.stats(error_df$std_flux_error[error_df$site==sites[1]])$stats[c(1,5)])/2,
-                                       xmin = 4.75,
-                                       xmax = 4.75))
-
-  g2 <- ggplotGrob(ggplot(error_df[error_df$site==sites[2],], aes(x=factor(LeadTime), y=std_flux_error, fill = flow_class)) +
-                     geom_hline(yintercept = 0,
-                                linetype = 'dashed') +
-                     geom_boxplot(outlier.shape = NA,
-                                  width = 0.7) +
-                     scale_fill_manual(name = 'flow_class',
-                                       values = c('high' = fig_config$flow_class$high,
-                                                  'med' = fig_config$flow_class$med,
-                                                  'low' = fig_config$flow_class$low),
-                                       labels = c(expression('High Flow '('>75'^'th')),
-                                                  expression('Medium Flow '('25'^'th'-'75'^'th')),
-                                                  expression('Low Flow '('<25'^'th')))) +
-                     theme_classic()+
-                     ylim(boxplot.stats(error_df$std_flux_error[error_df$site==sites[2]])$stats[c(1,5)]) +
-                     scale_x_discrete(limits = rev(levels(factor(error_df$LeadTime[error_df$site==sites[2]])))) +
-                     theme(axis.title.x = element_blank(),
-                           legend.position = c(.9,.8),
-                           legend.title = element_blank(),
-                           legend.text.align = 0,
-                           plot.margin = unit(c(1,3,1,1),'lines')) +
-                     ylab(expression('Relative flux error'~(('predict - obs')~'/'~'obs')))+
-                     annotation_custom(grob = textGrob(label = sites[2], hjust = 0, rot = 270),
-                                       ymin = diff(boxplot.stats(error_df$std_flux_error[error_df$site==sites[2]])$stats[c(1,5)])/2,
-                                       ymax = diff(boxplot.stats(error_df$std_flux_error[error_df$site==sites[2]])$stats[c(1,5)])/2,
-                                       xmin = 4.75,
-                                       xmax = 4.75))
-
-  g3 <- ggplotGrob(ggplot(error_df[error_df$site==sites[3],], aes(x=factor(LeadTime), y=std_flux_error, fill = flow_class)) +
-                     geom_hline(yintercept = 0,
-                                linetype = 'dashed') +
-                     geom_boxplot(outlier.shape = NA,
-                                  width = 0.7) +
-                     scale_fill_manual(name = 'flow_class',
-                                       values = c('high' = fig_config$flow_class$high,
-                                                  'med' = fig_config$flow_class$med,
-                                                  'low' = fig_config$flow_class$low),
-                                       labels = c('High Flow', 'Medium Flow', 'Low Flow')) +
-                     theme_classic()+
-                     ylim(boxplot.stats(error_df$std_flux_error[error_df$site==sites[3]])$stats[c(1,5)]) +
-                     scale_x_discrete(limits = rev(levels(factor(error_df$LeadTime[error_df$site==sites[3]])))) +
-                     xlab('Lead Time (days)') +
-                     theme(legend.position = 'none',
-                           axis.title.y = element_blank(),
-                           plot.margin = unit(c(1,3,1,1),'lines'))+
-                     annotation_custom(grob = textGrob(label = sites[3], hjust = 0, rot = 270),
-                                       ymin = diff(boxplot.stats(error_df$std_flux_error[error_df$site==sites[3]])$stats[c(1,5)])/2,
-                                       ymax = diff(boxplot.stats(error_df$std_flux_error[error_df$site==sites[3]])$stats[c(1,5)])/2,
-                                       xmin = 4.75,
-                                       xmax = 4.75))
-
-  g <- rbind(g1, g2, g3, size='first')
-  g$widths <- grid::unit.pmax(g1$widths, g2$widths, g3$widths)
-  g$layout$clip[g$layout$name=='panel'] <- 'off' # so site labels don't get cut off
-
-  # windows()
-  # grid.draw(g)
 
   # save and post to Drive
   fig_file <- as_data_file(fig_ind)
-  png(fig_file, width = 10, height = 7, units = 'in',res = 600); grid::grid.draw(g); dev.off()
+  png(fig_file, width = 7, height = 7, units = 'in',res = 600); grid::grid.draw(g); dev.off()
   gd_put(remote_ind=fig_ind, local_source=fig_file, config_file=config_file)
 }
